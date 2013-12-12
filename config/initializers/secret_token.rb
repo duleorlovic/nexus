cde83ae88da13178dce7375f621318b2d3e8222f
dusan <dusan@orlovic-dell.(none)>
2013-12-12 21:42:48 +0200

    adding bare rails and spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nexus::Application.config.secret_key_base = '9ee6ba0a6fcc73006ee00bce38110d5a98f7286ccebaef9a555a9be450f5af7bfac95c91475e448f4a3fec4438d911978c399dcf10c223769a55f400084d13c2'
